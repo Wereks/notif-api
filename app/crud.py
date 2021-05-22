d65d9648e0f2f39f7228940c6070c92767e60435
wereks <mateusz.zdon@student.uj.edu.pl>
2021-05-22 21:29:10 +0200

    Changed most of the ORM queries to SQL expressions

--- app/crud.py
+++ app/crud.py
@@ -1,33 +1,37 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
-
-def get_message(db: Session, message_id: int):
-    return db.query(models.Message).filter(models.Message.id == message_id).first()
+from .models import Message, User
+from . import schemas
 
 def read_message(db: Session, message_id: int):
-    if db_message := get_message(db, message_id):
-        return_message = schemas.Message(**vars(db_message))
-        db_message.views = db_message.views + 1
+    req = Message.__table__.update().\
+            where(Message.id == message_id).\
+            values(views=Message.views + 1).\
+            returning(Message)        
+    msg = db.execute(req).first()
+    db.commit()
+
+    if msg:
+        msg = schemas.Message(**msg)
+        msg.views = msg.views - 1
+
+    return msg
+
+def read_messages(db: Session, skip: int = 0, limit: int = 100):    
+    messages = db.execute(Message.__table__.select().order_by(Message.id).offset(skip).limit(limit)).fetchall()
+
+    if messages:
+        min_id, max_id = messages[0].id, messages[-1].id
+        req = Message.__table__.update().\
+                where(Message.id >= min_id, Message.id <= max_id).\
+                values(views=Message.views + 1)
+        db.execute(req)
         db.commit()
-        db.refresh(db_message)
-        return return_message
 
-    return None
-
-def read_messages(db: Session, skip: int = 0, limit: int = 100):
-    messages = db.query(models.Message).order_by(models.Message.id).offset(skip).limit(limit).all()
-    return_messages = [schemas.Message(**vars(db_message)) for db_message in messages]
-
-    #TODO: slow, faster method needed
-    for msg in messages:
-        msg.views = msg.views + 1
-    db.commit()    
-
-    return return_messages
+    return messages
 
 def create_message(db: Session, message: schemas.MessageCreate):
-    db_message = models.Message(text=message.text)
+    db_message = Message(text=message.text)
     db.add(db_message)
     db.commit()
     db.refresh(db_message)
@@ -35,23 +39,22 @@ def create_message(db: Session, message: schemas.MessageCreate):
     return db_message
    
 def update_message_text(db: Session, message_id: int, message: schemas.MessageCreate):
-    if db_message := get_message(db, message_id):
-        db_message.text = message.text
-        db_message.views = 0
-        db.commit()
-        db.refresh(db_message)
-        return db_message
-    
-    return None
+    req = Message.__table__.update().\
+            where(Message.id == message_id).\
+            values(views=0, text=message.text).\
+            returning(Message)
+    msg = db.execute(req).first()
+    db.commit()
+    return msg
 
 def delete_message(db: Session, message_id: int):
-    if db_message := get_message(db, message_id):
-        db.delete(db_message)
-        db.refresh(db_message)
-        db.commit()
-        return db_message
+    req = Message.__table__.delete().\
+            where(Message.id == message_id).\
+            returning(Message)
+    msg = db.execute(req).first()
+    db.commit()
+    return msg
 
-    return None
 
 def get_user(db: Session, user: schemas.User):
-    return db.query(models.User).filter(models.User.username == user.username).first()
+    return db.query(User).filter(User.username == user.username).first()
